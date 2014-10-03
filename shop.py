3274efec417c5b4af83a5f31935a22267edd5eef
resteve <resteve@zikzakmedia.com>
2014-10-03 12:59:41 +0200

    Import/Export galatea methods (raise user error)

--- shop.py
+++ shop.py
@@ -17,6 +17,14 @@ class SaleShop:
     __name__ = 'sale.shop'
 
     @classmethod
+    def __setup__(cls):
+        super(SaleShop, cls).__setup__()
+        cls._error_messages.update({
+            'not_import': 'Functions import/export in Galatea Shops are ' \
+            'not necessary',
+        })
+
+    @classmethod
     def get_shop_app(cls):
         res = super(SaleShop, cls).get_shop_app()
         res.append(('galatea','Galatea'))
@@ -25,3 +33,12 @@ class SaleShop:
     @staticmethod
     def default_esale_shop_app():
         return 'galatea'
+
+    def import_orders_galatea(self, shop):
+        self.raise_user_error('not_import')
+
+    def export_state_galatea(self, shop):
+        """Export State Sale whitout app don't available
+        :param shop: Obj
+        """
+        self.raise_user_error('not_import')
