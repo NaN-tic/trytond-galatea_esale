ff6a358be223fa026c9d7e5ef4b92227a7c55e4a
resteve <resteve@zikzakmedia.com>
2014-10-03 13:00:26 +0200

    pyflakes

--- shop.py
+++ shop.py
@@ -1,13 +1,7 @@
 #This file is part galatea_esale module for Tryton.
 #The COPYRIGHT file at the top level of this repository contains 
 #the full copyright notices and license terms.
-from trytond.model import ModelView, ModelSQL, fields
-from trytond.transaction import Transaction
-from trytond.pool import Pool, PoolMeta
-from trytond.pyson import Eval
-
-from decimal import Decimal
-import time
+from trytond.pool import PoolMeta
 
 __all__ = ['SaleShop']
 __metaclass__ = PoolMeta
