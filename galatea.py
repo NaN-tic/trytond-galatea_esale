590013146076263c892849111c4f4291e32783a6
resteve <resteve@zikzakmedia.com>
2015-03-04 17:56:24 +0100

    Domain invoice/delivery galatea user related to party

--- galatea.py
+++ galatea.py
@@ -32,8 +32,10 @@ class GalateaWebSite:
 class GalateaUser:
     __name__ = "galatea.user"
     invoice_address = fields.Many2One('party.address', 'Invoice Address',
+        domain=[('party', '=', Eval('party')), ('invoice', '=', True)],
         help='Default Invoice Address')
     shipment_address = fields.Many2One('party.address', 'Shipment Address',
+        domain=[('party', '=', Eval('party')), ('delivery', '=', True)],
         help='Default Shipment Address')
     b2b = fields.Boolean('B2B',
         help='Allow views or data from B2B customers')
