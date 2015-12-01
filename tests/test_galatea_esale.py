9005dc4d829f96bb2c5f82200f35e6f4fe1a1f05
resteve <resteve@zikzakmedia.com>
2015-12-01 11:08:17 +0100

    Tests

--- tests/test_galatea_esale.py
+++ tests/test_galatea_esale.py
@@ -1,25 +1,14 @@
-#!/usr/bin/env python
-# This file is part galatea_esale module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the galatea_esale module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class GalateaEsaleTestCase(unittest.TestCase):
-    'Test Galatea eSale module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('galatea_esale')
-
-    def test0005views(self):
-        'Test views'
-        test_view('galatea_esale')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+class GalateaEsaleTestCase(ModuleTestCase):
+    'Test Galatea Esale module'
+    module = 'galatea_esale'
 
 
 def suite():
