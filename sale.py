d20b9173739f06c66206ff63006ed2cf8e205211
resteve <resteve@zikzakmedia.com>
2016-02-23 17:59:35 +0100

    Remove sale cart dependency

--- sale.py
+++ sale.py
@@ -3,8 +3,9 @@
 # the full copyright notices and license terms.
 from trytond.pool import PoolMeta
 from trytond.model import fields
+from trytond.pyson import Eval, Not, Bool, And
 
-__all__ = ['Sale']
+__all__ = ['Sale', 'SaleLine']
 __metaclass__ = PoolMeta
 
 
@@ -12,3 +13,32 @@ class Sale:
     __name__ = 'sale.sale'
     galatea_user = fields.Many2One('galatea.user', 'Galatea User',
          readonly=True)
+
+
+class SaleLine:
+    __name__ = 'sale.line'
+    sid = fields.Char('Session')
+    galatea_user = fields.Many2One('galatea.user', 'Galatea User', readonly=True)
+    product_id = fields.Function(fields.Integer('Product ID'), 'get_product_id')
+    template_id = fields.Function(fields.Integer('Template ID'), 'get_template_id')
+    shop = fields.Many2One('sale.shop', 'Shop', domain=[
+        ('id', 'in', Eval('context', {}).get('shops', [])),
+        ])
+
+    @classmethod
+    def __setup__(cls):
+        super(SaleLine, cls).__setup__()
+        cls.party.states['required'] = And((Not(Bool(Eval('sid')))), cls.party.states['required'])
+        cls.party.depends.append('sid')
+
+    def get_product_id(self, name):
+        '''Return product ID'''
+        if self.product:
+            return self.product.id
+        return None
+
+    def get_template_id(self, name):
+        '''Return template ID'''
+        if self.product:
+            return self.product.template.id
+        return None
