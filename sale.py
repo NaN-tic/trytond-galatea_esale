0a0c792a6faf0624626dc9941da5a5825379c8df
Raimon Esteve <raimon@nan-tic.com>
2019-02-06 17:45:32 +0100

    Allow add extra lines when create a new sale #036760

--- sale.py
+++ sale.py
@@ -23,6 +23,20 @@ class Sale:
         '''Overwrite this method to add more fields in sale object from request.form.data'''
         return self
 
+    def get_esale_lines(self):
+        '''Return sale lines without shipment cost lines'''
+        return [l for l in self.lines if (
+            l.product and l.product.esale_available and (
+                l.shipment_cost is None or l.shipment_cost == 0))]
+
+    def _get_extra_lines(self):
+        """
+        Return extra lines to will be created.
+        This method will be overwritten by other modules.
+
+        """
+        return []
+
 
 class SaleLine:
     __metaclass__ = PoolMeta
